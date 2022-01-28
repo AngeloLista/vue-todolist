9ca5decc1cac3021d6b8d6f3f0ec6e8b68fc2002
AngeloLista <abstractplayer@gmail.com>
2022-01-28 16:28:34 +0100

    created tasks array

---
+++ js/style.js
@@ -0,0 +1,17 @@
+console.log('JS OK');
+
+Vue.config.devtools = true;
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+        tasks: [
+            { text: 'Farina', done: true},
+            { text: 'Pasta', done: false},
+            { text: 'Noce moscata', done: false},
+            { text: 'Verdure', done: false},
+            { text: 'Acqua', done: false},
+            { text: 'Sale', done: false},
+        ]
+    }
+})
