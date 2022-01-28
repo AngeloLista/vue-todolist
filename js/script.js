787be81e584a1228b9dd69337c4ae2fe8e06e571
AngeloLista <abstractplayer@gmail.com>
2022-01-28 16:59:14 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log('JS OK');
+
+Vue.config.devtools = true;
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+        tasks: [
+            { text: 'Farina', done: true},
+            { text: 'Pasta', done: false},
+            { text: 'Noce moscata', done: false},
+            { text: 'Verdure', done: false},
+            { text: 'Acqua', done: false},
+            { text: 'Sale', done: false},
+        ]
+    },
+    methods: {
+
+    }
+
+})
